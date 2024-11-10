474007d97190b100d4644c1f4c971764b0461333
anderson <andersonbrenotec@gmail.com>
2024-11-09 21:31:36 -0300

    Criando repositório.

---
+++ src/main/java/com/aflweb/aflcommerce/AflcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.aflweb.aflcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AflcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AflcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aflweb/aflcommerce/AflcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aflweb.aflcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AflcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
